4b149635b296a5e9ab06ea6ebb316d5ed2689e08
hariamy <hariamymv@hotmail.com>
2019-11-19 17:59:03 -0300

    Correçoes nas rotas

--- web_server/index.js
+++ web_server/index.js
@@ -2,21 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const porta = 3001
 
 // Iniciando o APP
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Iniciando o DB
-mongoose.connect(
-	'mongodb+srv://hariamy:HARIAMY@clusterelas-oqljd.mongodb.net/test?retryWrites=true&w=majority ',  
-	{ useNewUrlParser: true },
-    err => { if (err) return console.error(err); }
-);
 requireDir('./src/models');
 
 // Rotas
 app.use('/api', require('./src/routes'));
 
-app.listen(3001);
+app.listen(porta);
+
+// Iniciando o DB
+mongoose.connect(
+	'mongodb://200.19.179.129:27017/web',  
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+    err => { if (err) return console.log("ih rapaz deu rum :("); console.error(err); }
+
+).then(
+    (err) => { 
+        if (!err) { 
+            console.log("Conexão realizada com sucesso!"); 
+            console.log("localhost:"+porta+"/api")
+        }
+    }
+);
